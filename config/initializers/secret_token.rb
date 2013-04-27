3162d86c6af5748a1bde6e5f2ea5b92240855d67
Jorge Barnaby <jorge.barnaby@gmail.com>
2013-04-27 11:15:41 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BigdataHack::Application.config.secret_token = '75e115661d8f0c439e4b60e9a51a528377df3ca56bd45ff1f8ecea1353a4edc1b4b35ca34f06d7a830cdaaa21893c87f6222f8b79bea1a1a7e08855ed0376425'
